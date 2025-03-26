49681f739f0490b769a573c113dd3274c639f603
nikita <nikitabelkin@pp-grup.ru>
2025-03-26 15:13:41 +0500

    ALLOWED_HOSTS - all

--- PPWeb/settings.py
+++ PPWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yfmzd&n(m_k2ye^*yper*93kl47c*+l5xdpd%i7*0_(f^gg08p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
